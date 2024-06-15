caacd09f58762b8bd192b40adac883047eddf7ed
AbnerHenriq <abnergalerarodrigs@hotmail.com>
2024-06-15 19:13:43 -0300

    feat: saving and config in duckdb

--- requests/get_brawlers.py
+++ requests/get_brawlers.py
@@ -1,5 +1,6 @@
 import requests 
 import pandas as pd
+import duckdb
 
 url = 'https://api.brawlstars.com/v1/brawlers'
 headers = { 
@@ -12,6 +13,17 @@ if response.status_code == 200:
     data = response.json()
     df = pd.DataFrame(data["items"])
     print(df)
+
+    # Open connection
+    con = duckdb.connect('database/brawnstars.duckdb')
+
+    # Insert dataframe
+    con.execute("CREATE TABLE IF NOT EXISTS brawlers AS SELECT * FROM df")
+    con.execute("INSERT INTO brawlers SELECT * FROM df")
+
+    # Close connection
+    con.close()
+
 else:
     print(f'Erro na requisição: {response.status_code}')
     print(response.text)
